becbebda1d0ed361c3c739ae340cb00d0f78301a
Полина Белякаева <polinabelakaeva@MBP-Polina.modem.local>
2023-01-09 23:43:46 +0300

    Fixed error in brain-gcd

--- src/games/find-greatest-common-divisor.js
+++ src/games/find-greatest-common-divisor.js
@@ -5,27 +5,32 @@ const description = 'Find the greatest common divisor of given numbers.';
 
 const checkGreatestCommonDivisor = (first, second) => {
   let result;
+  let firstNumber = first;
+  let secondNumber = second;
 
-  if (first === second) {
-    result = first;
+  if (firstNumber === secondNumber) {
+    result = firstNumber;
   }
 
-  while (first !== second) {
-    if (first > second) {
-      result = first - second;
+  while (firstNumber !== secondNumber) {
+    if (firstNumber > secondNumber) {
+      firstNumber -= secondNumber;
+      result = firstNumber;
     } else {
-      result = second - first;
+      secondNumber -= firstNumber;
+      result = secondNumber;
     }
   }
 
   return String(result);
 };
-const getQuestionAndAnswer = () => {
 
+const getQuestionAndAnswer = () => {
   const firstNumber = getRandomNumber(1, 50);
   const secondNumber = getRandomNumber(1, 50);
-  const question = ` ${firstNumber} ${secondNumber}`;
+  const question = `${firstNumber} ${secondNumber}`;
   const correctAnswer = checkGreatestCommonDivisor(firstNumber, secondNumber);
+  
 
   return [question, correctAnswer];
 };
